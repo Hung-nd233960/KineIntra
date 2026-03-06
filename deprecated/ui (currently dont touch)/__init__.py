e39b6b05d1266713e22c961ea1a5431a0d4935e1
Hung-nd233960 <hung1qqo@gmail.com>
2026-03-06 10:57:00 +0700

    Update Project Structure

--- deprecated/ui (currently dont touch)/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-UI module for BioMechanics Microprocessor Control
-
-Contains PyQt6 application components.
-"""
-
-from ui.pyqt_app import BioMechanicsApp, main
-
-__all__ = ["BioMechanicsApp", "main"]
